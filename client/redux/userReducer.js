7b65c30f4117b2b4d0f930f039a0aeeebd4126ba
YuriiShchyrba <yurashchyrba@gmail.com>
2022-04-04 16:11:17 -0400

    user Reducer update

--- client/redux/userReducer.js
+++ client/redux/userReducer.js
@@ -1,3 +1,7 @@
+const GET_USER = "GET_USER";
+
+export const getUserAction = (data) => ({ type: GET_USER, data: data });
+
 const initialState = {
   fullName: "",
   password: "",
@@ -7,6 +11,15 @@ const initialState = {
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_USER: {
+      return {
+        ...state,
+        fullName: action.data.fullName,
+        password: action.data.password,
+        email: action.data.email,
+        cohortName: action.data.cohortName,
+      };
+    }
     default:
       return state;
   }
