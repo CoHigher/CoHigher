c31a244a154e3bad3d70d4769e030d3273b639d2
YuriiShchyrba <yurashchyrba@gmail.com>
2022-04-05 11:55:45 -0400

    feat: testing logic

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,9 @@
 import React  from "react";
 import JobsContainer from "./container/JobsContainer";
-import photo from './assets/img-academy-oqxuxY.jpeg';
 
 const App = (props) => {
-  props.store
   return <div>
-    <img src={photo}/>
+    <JobsContainer />
   </div>;
 };
 
