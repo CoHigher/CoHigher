2cfbfd0c7a6dd32356fc83a8912786d6bd1e325b
Yasir Choudhury <yasir2493@gmail.com>
2022-04-04 21:34:46 -0400

    model file

---
+++ server/controllers.js
@@ -0,0 +1,14 @@
+const db = require('/model');
+const userJobsController = {};
+
+userJobsController.signup = (req, res, next) => {
+ 
+};
+
+userJobsController.login = (req, res, next) => {
+ 
+  
+};
+
+
+module.exports = userJobsController;
